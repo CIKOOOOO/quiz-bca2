2845f817e6a6cd3302acfb08d550626e31285fd0
Hector <32627138+lewdxample@users.noreply.github.com>
2019-03-27 16:32:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>. -->
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="/css/style.css">
 	<script src="https://www.gstatic.com/firebasejs/5.9.1/firebase.js"></script>
 		<script>
 		  // Initialize Firebase
@@ -48,7 +48,7 @@
 		<div class="container2">
 			<progress value="0" max="15" id="progressBar"></progress>
 			<div class="box">
-				<img src="bca.png" width="100px" height="100px" alt="qimg">
+				<img src="/res/bca.png" width="100px" height="100px" alt="qimg">
 				<div class="question">Warna logo BCA?</div> 
 	
 				<div class="ansrow-1">
@@ -62,7 +62,7 @@
 			</div>
 	
 			<div class="box">
-					<img src="wheel1.png" width="100px" height="100px" alt="qimg">
+					<img src="/res/wheel1.png" width="100px" height="100px" alt="qimg">
 					<div class="question">Gambar logo Go-Jek?</div> 
 		
 					<div class="ansrow-1">
